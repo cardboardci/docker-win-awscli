904d37eddeb0d056b63d9eabf25bee2b10bea2f1
jrbeverly <jrbeverly@live.com>
2019-01-13 12:22:34 -0500

    Reorder the code for building

--- build/build.ps1
+++ build/build.ps1
@@ -2,6 +2,9 @@ $ErrorActionPreference = 'Stop';
 Write-Host Starting build
 $image = "quay.io/cardboardci/awscli:windows"
 
+docker build --pull -t $image -f Dockerfile .
+docker images
+
 if (! (Test-Path Env:\CI_REGISTRY)) {
   Write-Host "No registry provided. Skip publishing."
   exit 0
@@ -20,11 +23,7 @@ if (! (Test-Path Env:\CI_REGISTRY_PASSWORD)) {
 try {
     docker login -u "$env:CI_REGISTRY_USER" -p "$env:CI_REGISTRY_PASSWORD" $env:CI_REGISTRY
 } catch {
-    
 }
 
-# docker build --pull -t $image -f Dockerfile .
-# docker images
-
-# Write-Host Starting deploy
-# docker push "$image"
+Write-Host Starting deploy
+docker push "$image"

4159d51f081cdf22598fdb8f8bbec1c00dd27eb4
jrbeverly <jrbeverly@live.com>
2019-01-13 11:46:06 -0500

    Setup a windows build process

--- build/build.ps1
+++ build/build.ps1
@@ -1,10 +1,26 @@
 $ErrorActionPreference = 'Stop';
 Write-Host Starting build
+$image = "cardboardci/awscli:windows"
 
-if ($isWindows) {
-  docker build --pull -t whoami -f Dockerfile .
-} else {
-  docker build -t whoami .
+docker build --pull -t $image -f Dockerfile .
+docker images
+
+if (! (Test-Path Env:\CI_REGISTRY)) {
+  Write-Host "No registry provided. Skip publishing."
+  exit 0
 }
 
-docker images
+if (! (Test-Path Env:\CI_REGISTRY_USER)) {
+  Write-Host "No registry user provided. Skip publishing."
+  exit 0
+}
+
+if (! (Test-Path Env:\CI_REGISTRY_PASSWORD)) {
+  Write-Host "No password provided. Skip publishing."
+  exit 0
+}
+
+docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" $CI_REGISTRY
+
+Write-Host Starting deploy
+docker push "$image"

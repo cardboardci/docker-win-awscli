e9f9ae301b05050faa4814ff8415fdf37b64d368
jrbeverly <jrbeverly@live.com>
2019-02-01 23:25:28 -0500

    Configure setup

--- build/Setup.ps1
+++ build/Setup.ps1
@@ -1,6 +1,8 @@
 New-Item -Path "$HOME\.docker" -Type Directory -Force
 Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
 Set-Content -Path "$HOME\.docker\config.json" -Value ( @{ "experimental" = "enabled" } | ConvertTo-Json )
+Get-Content -Path "C:\ProgramData\Docker\config\daemon.json"
+Get-Content -Path "$HOME\.docker\config.json"
 docker version
 restart-service *docker*
 Start-Sleep -Seconds 5

57664feff4d500e6eb6ae7f92fe48c01e8aefc13
jrbeverly <jrbeverly@live.com>
2019-02-01 23:13:38 -0500

    Establish experimental

--- build/Run.ps1
+++ build/Run.ps1
@@ -1,11 +1,11 @@
-
 New-Item -Path "$HOME\.docker" -Type Directory -Force
 Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
 Set-Content -Path "$HOME\.docker\config.json" -Value ( @{ "experimental" = "enabled" } | ConvertTo-Json )
+docker version
 restart-service *docker*
 Start-Sleep -Seconds 5
-
 docker version
+docker manifest --help 
 
 Write-Host "Starting build"
 # docker build --pull -t cardboardci/awscli:windows -f Dockerfile .

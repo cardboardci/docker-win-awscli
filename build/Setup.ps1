f041d399f5124bc956ef9947c1916a8069afdbe4
jrbeverly <jrbeverly@live.com>
2019-02-01 23:35:58 -0500

    Configure CLI

--- build/Setup.ps1
+++ build/Setup.ps1
@@ -1,6 +1,14 @@
 New-Item -Path "$HOME\.docker" -Type Directory -Force
 Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
-Set-Content -Path "$HOME\.docker\config.json" -Value ( @{ "experimental" = "enabled" } | ConvertTo-Json )
+
+$cli = @{
+  "HttpHeaders": @{
+    "User-Agent": "Docker-Client/18.04.0-ce (linux)"
+  },
+  "experimental": "enabled"
+}
+
+Set-Content -Path "$HOME\.docker\config.json" -Value ( $cli | ConvertTo-Json )
 Get-Content -Path "C:\ProgramData\Docker\config\daemon.json"
 Get-Content -Path "$HOME\.docker\config.json"
 docker version

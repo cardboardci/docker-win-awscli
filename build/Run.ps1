03acdb83cc14013c80cd72108d19a628619298e8
jrbeverly <jrbeverly@live.com>
2019-02-01 22:39:26 -0500

    Test the process of building and deploying

--- build/Run.ps1
+++ build/Run.ps1
@@ -1,25 +1,21 @@
-Function Enable-DockerExperimental() {
-  New-Item -Path "$HOME\.docker" -Type Directory -Force
-  Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
-  Set-Content -Path "$HOME\.docker\config.json" -Value ( @{ "experimental" = "enabled" } | ConvertTo-Json )
-  restart-service *docker*
-  Start-Sleep -Seconds 5
-}
 
-Enable-DockerExperimental
+New-Item -Path "$HOME\.docker" -Type Directory -Force
+Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
+Set-Content -Path "$HOME\.docker\config.json" -Value ( @{ "experimental" = "enabled" } | ConvertTo-Json )
+restart-service *docker*
+Start-Sleep -Seconds 5
 
 Write-Host "Starting build"
-docker manifest inspect cardboardci/awscli:edge
-# docker manifest create 
-#     cardboardci/awscli:latest 
-#     cardboardci/awscli:linux 
-#     cardboardci/awscli:windows
+docker build --pull -t cardboardci/awscli:windows -f Dockerfile .
+docker push cardboardci/awscli:windows
+docker manifest create 
+    cardboardci/awscli:edge 
+    cardboardci/awscli:linux 
+    cardboardci/awscli:windows
+docker manifest push cardboardci/awscli:latest 
 
-# docker manifest push cardboardci/awscli:latest 
 
-# docker login -u "$env:CI_REGISTRY_USER" -p "$env:CI_REGISTRY_PASSWORD" $env:CI_REGISTRY
 # docker build --pull -t $image -f Dockerfile .
 # docker images
 
 # Write-Host Starting deploy
-# docker push "$image"

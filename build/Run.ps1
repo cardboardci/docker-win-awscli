e68ed52be05c8a201b2aa82b73ebdfcb706d9cbc
jrbeverly <jrbeverly@live.com>
2019-02-01 22:14:24 -0500

    Explore the edge build

--- build/Run.ps1
+++ build/Run.ps1
@@ -1,13 +1,21 @@
-Write-Host "Starting build"
-New-Item -Path "$HOME\.docker" -Type Directory -Force
-Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
-Set-Content -Path "$HOME\.docker\config.json" -Value ( @{ "experimental" = "enabled" } | ConvertTo-Json )
+Function Enable-DockerExperimental() {
+  New-Item -Path "$HOME\.docker" -Type Directory -Force
+  Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
+  Set-Content -Path "$HOME\.docker\config.json" -Value ( @{ "experimental" = "enabled" } | ConvertTo-Json )
+  restart-service *docker*
+  Start-Sleep -Seconds 5
+}
 
-docker version
-restart-service *docker*
-Start-Sleep -Seconds 5
-docker version
-docker manifest --help 
+Enable-DockerExperimental
+
+Write-Host "Starting build"
+docker manifest inspect cardboardci/awscli:edge
+# docker manifest create 
+#     cardboardci/awscli:latest 
+#     cardboardci/awscli:linux 
+#     cardboardci/awscli:windows
+
+# docker manifest push cardboardci/awscli:latest 
 
 # docker login -u "$env:CI_REGISTRY_USER" -p "$env:CI_REGISTRY_PASSWORD" $env:CI_REGISTRY
 # docker build --pull -t $image -f Dockerfile .

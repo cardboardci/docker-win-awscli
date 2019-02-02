8cf33302356e81f48b2a6a5d4b36dd81c5abc2ef
jrbeverly <jrbeverly@live.com>
2019-02-01 21:42:45 -0500

    Update the codebase

--- build/Run.ps1
+++ build/Run.ps1
@@ -1,21 +1,6 @@
 $ErrorActionPreference = 'Ignore';
 Write-Host "Starting build"
 
-if (! (Test-Path Env:\CI_REGISTRY)) {
-  Write-Host "No registry provided. Skip publishing."
-  exit 0
-}
-
-if (! (Test-Path Env:\CI_REGISTRY_USER)) {
-  Write-Host "No registry user provided. Skip publishing."
-  exit 0
-}
-
-if (! (Test-Path Env:\CI_REGISTRY_PASSWORD)) {
-  Write-Host "No password provided. Skip publishing."
-  exit 0
-}
-
 Get-Content "C:\ProgramData\Docker\config\daemon.json"
 
 # docker login -u "$env:CI_REGISTRY_USER" -p "$env:CI_REGISTRY_PASSWORD" $env:CI_REGISTRY

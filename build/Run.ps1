1f73a5db34aef3888b16a86509a15771ea5fc963
jrbeverly <jrbeverly@live.com>
2019-02-01 21:45:15 -0500

    Explore Azure DevOps options

--- build/Run.ps1
+++ build/Run.ps1
@@ -1,7 +1,17 @@
-$ErrorActionPreference = 'Ignore';
 Write-Host "Starting build"
 
-Get-Content "C:\ProgramData\Docker\config\daemon.json"
+try {
+  Get-ChildItem "C:\"
+  Get-ChildItem "C:\ProgramData\"
+  Get-ChildItem "C:\ProgramData\Docker\"
+  Get-ChildItem "C:\ProgramData\Docker\config"
+  Get-Content "C:\ProgramData\Docker\config\daemon.json"
+} catch {
+  Write-Host "Could not find the file"
+}
+
+docker images
+docker manifest --help 
 
 # docker login -u "$env:CI_REGISTRY_USER" -p "$env:CI_REGISTRY_PASSWORD" $env:CI_REGISTRY
 # docker build --pull -t $image -f Dockerfile .

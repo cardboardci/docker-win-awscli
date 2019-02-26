8e8af1dceb0c468ef36265cac24436468d5b7a81
jrbeverly <jrbeverly@live.com>
2019-02-26 14:02:11 -0500

    Update the path

--- .docker/Setup.ps1
+++ .docker/Setup.ps1
@@ -1,5 +1,5 @@
 New-Item -Path "$HOME\.docker" -Type Directory -Force
-Move-Item -Destination "$HOME\.docker\config.json" -Path ".docker/config.json"
+Move-Item -Destination "$HOME\.docker\config.json" -Path "$PSScriptRoot/config.json"
 
 Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value ( @{ "experimental" = $true } | ConvertTo-Json )
 Get-Content -Path "C:\ProgramData\Docker\config\daemon.json"

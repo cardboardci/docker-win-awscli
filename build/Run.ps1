9cfee5d279bb205d9a05be59d86d8dcb3e4095de
jrbeverly <jrbeverly@live.com>
2019-02-26 13:11:21 -0500

    Update the build process

--- build/Run.ps1
+++ build/Run.ps1
@@ -1,9 +1,3 @@
 Write-Output "Starting build"
-# docker build --pull -t cardboardci/awscli:windows -f Dockerfile .
-# docker push cardboardci/awscli:windows
-docker manifest create
-    cardboardci/awscli:edge
-    cardboardci/awscli:linux
-    cardboardci/awscli:windows
-docker manifest push cardboardci/awscli:latest
-# docker build --pull -t $image -f Dockerfile .
+docker build --pull -t cardboardci/awscli:windows -f Dockerfile .
+docker push cardboardci/awscli:windows
